e1d2ee6d51e63a5531752d5a642732d4d93b3857
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-11-13 13:43:10 +0200

    added daily quran pages

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 import os
+import pytz
 import dotenv
+import asyncio
 import discord
 from discord.ext import commands
+from discord import Embed
 import azkar.azkar_util as azkar_util
+from datetime import datetime, timedelta
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,6 +16,9 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 
 PING_CHANNEL_ID = -1
 AZKAR_CHANNEL_ID = -1
+QURAN_CHANNEL_ID = -1
+
+QURAN_URL = "https://surahquran.com/img/pages-quran/page"
 
 
 @bot.event
@@ -53,6 +60,38 @@ async def set_azkar_channel(ctx, channel):
     AZKAR_CHANNEL_ID = int(channel.strip('<>#'))
     await ctx.send(f"Set azkar channel to {channel}")
 
+
+@bot.command(name="set-quran-channel")
+async def set_quran_channel(ctx, channel):
+    """This command sets the channel to send quran to"""
+    global QURAN_CHANNEL_ID
+    QURAN_CHANNEL_ID = int(channel.strip('<>#'))
+    await ctx.send(f"Set quran channel to {channel}")
+
+
+@bot.command(name="start-werd")
+async def start_werd(ctx, startingPage, numOfPages):
+    numOfPages = int(numOfPages)
+    startingPage = int(startingPage)
+    quranChannel = ctx.bot.get_channel(QURAN_CHANNEL_ID)
+    timezone = pytz.timezone('Asia/Riyadh')
+
+    while True:
+        now = datetime.now(timezone)
+
+        time = now.replace(hour=12, minute=0, second=0, microsecond=0)
+        if now > time:
+            time += timedelta(days=1)
+
+        sleep_duration = (time - now).total_seconds()
+
+        print(f"Sleeping for {sleep_duration} seconds until the next alarm.")
+        await asyncio.sleep(sleep_duration)
+
+        for i in range(startingPage, startingPage + numOfPages):
+            embed = Embed(title=f"Quran Page {i}")
+            embed.set_image(url=f"{QURAN_URL}{str(i).zfill(3)}.png")
+            await quranChannel.send(embed=embed)
 
 if __name__ == '__main__':
     try:
